0888c7d58a032fad3373ccc83b7bf26c7b3169a0
Nidhesh <nidheshpillai@icloud.com>
2018-04-19 15:22:30 +0100

    Created TTD for new feature

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -13,6 +13,7 @@ public class AccountTest {
 	Account Account1 = new Account("Nidhesh", "Pillai", "1001");
 	Account Account2 = new Account("Jason", "Hart", "1002");
 	Account Account3 = new Account("Callum", "McGregor", "1003");
+	Account Account4 = new Account("Nidhesh", "Pillai", "1004");
 
 	@Test
 	public void addAccountTest() {
@@ -27,12 +28,25 @@ public class AccountTest {
 
 	@Test
 	public void jSonTest() {
-		
+
 		Account test1 = new Account("Nidhesh", "Pillai", "1001");
 		service.addAccount(test1);
 		String output = "{\"1\":{\"firstName\":\"Nidhesh\",\"lastName\":\"Pillai\",\"accountNumber\":\"1001\"}}";
 		JSon gson = new JSon();
 		String jsonTest = gson.getJSon(service.getAccountMap());
 		assertEquals(jsonTest, output);
+	}
+
+	@Test
+	public void returnGivenName() {
+
+		service.addAccount(Account1);
+		service.addAccount(Account2);
+		service.addAccount(Account3);
+		service.addAccount(Account4);
+		Assert.assertEquals(service.getCountofAccountWithFirstName("Jason"), 1);
+		Assert.assertEquals(service.getCountofAccountWithFirstName("Calum"), 0);
+		Assert.assertEquals(service.getCountofAccountWithFirstName("Jason"), 1);
+		Assert.assertEquals(service.getCountofAccountWithFirstName("Nidhesh"), 2);
 	}
 }
